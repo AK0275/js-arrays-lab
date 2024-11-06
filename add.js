7844af24dbfb78c37fe0c5ab1933899da79b0172
AK0275 <abdullakhalid0275@gmail.com>
2024-11-06 11:32:17 +0300

    exercise 2 done

--- add.js
+++ add.js
@@ -5,3 +5,7 @@ console.log('Exercise 1 result;', foods);
 foods.push('pizza', 'cheeseburger')
 
 console.log('Exercise 2 result:', foods);
+
+foods.unshift('taco')
+
+console.log('Exercise 3 result:', foods);
