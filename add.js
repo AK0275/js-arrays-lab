e8e9d3529754244eb2aa85dbfc4bbcb4ad060016
AK0275 <abdullakhalid0275@gmail.com>
2024-11-06 12:12:48 +0300

    exercise 4 done

--- add.js
+++ add.js
@@ -9,3 +9,16 @@ console.log('Exercise 2 result:', foods);
 foods.unshift('taco')
 
 console.log('Exercise 3 result:', foods);
+
+let favFood = foods
+
+favFood.pop('pizza')
+
+favFood.unshift('pizza')
+
+favFood.forEach((favFood, index) => {
+
+    console.log(`${index}: ${favFood}`)
+})
+
+console.log('Exercise 4 result:', favFood);
